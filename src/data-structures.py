bb78e47d0cd5ddd871a38578c64431b3da1585c3
keyla <keyyleiva@gmail.com>
2023-10-02 14:58:55 -0400

    notes

--- src/data-structures.py
+++ src/data-structures.py
@@ -1,7 +1,48 @@
 # lists and list comprehension
+#lists are mutable 
+nums = [1, 2, 3, 4, 5]
+print(nums)
+#slice [starting index : ending index] .pop(5) to remove an index
+# nums.append(6)
+# print(nums)
+# nums.pop(4)
+# print(nums)
 
+# for num in nums:
+#     print(num)
+
+# nums_squared = []
+
+# nums_squared = [num * num for num in nums]
+# print(nums_squared)
+
+# nums_cubed = []
+
+# nums_cubed = [num ** 3 for num in nums if num % 2 == 0]
+# print(nums_cubed)
+
+# for num in nums: 
+#     if num % 2 == 0:
+#         nums_cubed.append(num * num * num)
 # dictionaries
+    #dictionaries are mutable 
 
+squares = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+squares [6] = 36
+print(squares)
 # sets
+    #no duplicates in a set. sets are mutable 
+
+plants = set()
+
+plants.add("tree")
+plants.add("bush")
+plants.add("flower")
+
+print(plants)
 
 # tuples
+    # are immuatble. 
+color = (9, 18, 200)
+print(color)
--- src/fundamentals.py
+++ src/fundamentals.py
@@ -2,10 +2,55 @@
     # string concatenation: "string1" + "string2"
     # f-string interpolation: f"{string1}{string2}"
 
+name = "Keyla"
+last_name = "Leiva"
+
+favorite_number = 4
+# print(name + "'s favorite number is " + str(favorite_number))
+# print(f"{name}'s favorite number is {favorite_number}")
+
 # conditional statements
+if name == "Keyla":
+    print("Hello Key")
+
+if name == "Tyler" and last_name == "Taylor":
+    print("Hello Tyler")
+else: 
+    print("you're not tyler")
+
+
+if favorite_number < 0:
+    print("the number is negative")
+elif favorite_number == 0:
+    print("the num is 0")
+else: 
+    print("the num is positive")
+
+print("the number is BIG") if favorite_number > 5 else print("the number is small")
 
 # loops and sequences
     # for loops
     # while loops
+# for i in range(10):
+#     print(i)
+# for i in range(5,10):
+#     print(i)
+# for i in range(5, 10, 2):
+#     print(i)
 
+counter = 0
+while counter < 10:
+    print(counter)
+    counter = counter + 1 
 # functions
+
+def prints_hello_world():
+    print("hello world")
+prints_hello_world()
+
+def adds_two_numbers(num1, num2):
+    sum = num1 + num2 
+    return sum 
+
+sum = adds_two_numbers(3, 4)
+print(sum)
